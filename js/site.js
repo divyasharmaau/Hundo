6d8b8e8e39d8c87d25109da72e5fa0f12ea1f830
ds <divyasharma.au@gmail.com>
2024-10-01 12:14:13 +1000

    Hundo Completed

--- js/site.js
+++ js/site.js
@@ -1,4 +1,59 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World!!");
+
+//get the values from the page
+//start or controller function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    // let numbers =[];
+
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        //call generate numbers
+        let numbers = genrateNumbers(startValue, endValue);
+         //call display numbers
+        displayNumbers(numbers);
+    }
+    else{
+        alert("You must enter an Integer");
+    }
+   
+}
+
+//generate numbers from startValue to endValue
+//logic function(s)
+function genrateNumbers(sValue, eValue){
+    let numbers = [];
+    //all numbers from start to end
+    for(let i=sValue; i<= eValue; i++){
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+//display the numbers and the even numbers bold
+//display or view functions
+
+function displayNumbers(numbers){
+
+    let templateRows ="";
+  
+    for(let i=0; i < numbers.length; i++){
+
+        let className = "even";
+       
+        let number = numbers[i];
+        if(number % 2 == 0){
+            className = "even";
+        }
+        else{
+            className ="odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`
+    }
+    
+    document.getElementById("results").innerHTML = templateRows;
 }
